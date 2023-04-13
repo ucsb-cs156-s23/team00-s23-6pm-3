a2f81469996a1a5e9af9c2a5c9a4e315a792d045
MrJacobEisner <mrjeisner@gmail.com>
2023-04-12 18:49:32 -0700

    JE - add page for Calgary

---
+++ frontend/src/main/pages/CalgaryPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function CalgaryPage() {
+    return (
+        <div>
+            <h1>Calgary, Alberta, Canada</h1>
+            <p>
+                Calgary is the largest city in Alberta and is known for its
+                amazing food scene with restaurants offering almost every
+                cuisine around the world. Rocky mountains and surreal landscapes
+                add charm to this place. Aside from picturesque locations and
+                amazing food, the city is also known for its winter sports,
+                having ideal spots for skiing and snowboarding.
+            </p>
+            <p>Added by Jacob E.</p>
+        </div>
+    );
+}
