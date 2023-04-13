fdd15d66e45d3ab5311c419796e6b3dc5667e754
MaximillianStewart <mstewart@ucsb.edu>
2023-04-13 15:18:47 -0700

    added santa barbara

---
+++ frontend/src/main/pages/SantaBarbaraPage.js
@@ -0,0 +1,15 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function SantaBarbaraPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Santa Barbara, CA</h1>
+        <p>
+         Santa Barbara, California is frickin lit
+        </p>
+        <p>Added by Max S.</p>
+      </div>
+    </BasicLayout>
+  )
+}
