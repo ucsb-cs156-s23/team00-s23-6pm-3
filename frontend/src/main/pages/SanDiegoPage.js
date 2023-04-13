88e8fdc6dcb44d6184561910be975804127d807d
Anshuman Dash <anshuman@ucsb.edu>
2023-04-12 18:34:14 -0700

    ad - added san diego page

--- frontend/src/main/pages/SanDiegoPage.js
+++ frontend/src/main/pages/SanDiegoPage.js
@@ -1,17 +1,21 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-export default function ArroyoGrandePage() {
+export default function SanDiegoPage() {
 	return (
 		<BasicLayout>
 			<div className="pt-2">
-				<h1>Arroyo Grande, CA</h1>
+				<h1>San Diego, CA</h1>
 				<p>
-					<b>Arroyo Grande, California</b> is a town in San Luis Obispo county, to
-					the southeast of San Luis Obispo.
-					The central business district is called the "Village", and is
-					adjacent to a park, where feral chickens roam.
+					<b>San Diego, California</b> is a coastal city located
+					in the southern part of California, United States. 
+					It is known for its sunny weather, stunning beaches, 
+					and laid-back lifestyle. San Diego is also home to 
+					numerous attractions, such as the San 
+					Diego Zoo, SeaWorld, and Legoland.
+					San Diego is a beautiful and dynamic city that offers
+					something for everyone.
 				</p>
-				<p>Added by Phill C.</p>
+				<p>Added by Anshuman D.</p>
 			</div>
 		</BasicLayout>
 	)
