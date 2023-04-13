67449b40b7837dab28d0358cc3c0d4437201d343
Amy Wang <ayw@umail.ucsb.edu>
2023-04-13 15:24:15 -0700

    AW - add page for Los Angeles

---
+++ frontend/src/main/pages/LosAngelesPage.js
@@ -0,0 +1,18 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function LosAngelesPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Los Angeles, CA</h1>
+        <p>
+         <b>Los Angeles, California</b> is a city in Los Angeles County.
+         It is located in southern California.
+         It is the heart of the nation's film industry and contains a lot of icons such as
+         many major movie studios, the iconic Hollywood sign and the Hollywood Walk of Fame.
+        </p>
+        <p>Added by Amy W.</p>
+      </div>
+    </BasicLayout>
+  )
+}
