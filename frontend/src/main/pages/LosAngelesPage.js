f2ed49d24fda9058465fa07b70460e3e3b23b694
Amy Wang <ayw@umail.ucsb.edu>
2023-04-13 17:51:53 -0700

    AW - add page for Los Angeles

--- frontend/src/main/pages/LosAngelesPage.js
+++ frontend/src/main/pages/LosAngelesPage.js
@@ -10,6 +10,9 @@ export default function LosAngelesPage() {
          It is located in southern California.
          It is the heart of the nation's film industry and contains a lot of icons such as
          many major movie studios, the iconic Hollywood sign and the Hollywood Walk of Fame.
+         It is the second most popolous city in the United States, with a population of
+         around 3.8 million as of 2023.
+         It hosted the 1932 and 1984 Summer Olympics, and has arrangements to host again in 2028.
         </p>
         <p>Added by Amy W.</p>
       </div>
