4efa57f6fb9ea0b32dfbc013eff955314d2a20be
nhs-wobuzhidao <anoushka@ucsb.edu>
2023-04-16 20:45:46 -0700

    AS - add page for Solvang

---
+++ frontend/src/main/pages/SolvangPage.js
@@ -0,0 +1,17 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function SolvangPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Solvang, CA</h1>
+        <p>
+         <b>Solvang, California</b> is a town in San Ynez county. It is
+         known for its Danish architecture, bakeries, and antique shops, 
+         as well as its storybook air. 
+        </p>
+        <p>Added by Anoushka S.</p>
+      </div>
+    </BasicLayout>
+  )
+}
